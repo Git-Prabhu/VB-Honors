0c2b0576658a03b30ec7fa295618c7f0d31fa518
Git-Prabhu <pcube@hotmail.com>
2018-07-19 17:03:53 -0400

    Add files via upload

---
+++ Age_DOB_Calculator.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblDOB = New System.Windows.Forms.Label()
+        Me.lblToday = New System.Windows.Forms.Label()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DateTimePicker2 = New System.Windows.Forms.DateTimePicker()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.btnCalc)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker2)
+        Me.GroupBox1.Controls.Add(Me.DateTimePicker1)
+        Me.GroupBox1.Controls.Add(Me.lblToday)
+        Me.GroupBox1.Controls.Add(Me.lblDOB)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 24)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(438, 236)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'lblDOB
+        '
+        Me.lblDOB.AutoSize = True
+        Me.lblDOB.Location = New System.Drawing.Point(9, 47)
+        Me.lblDOB.Name = "lblDOB"
+        Me.lblDOB.Size = New System.Drawing.Size(69, 13)
+        Me.lblDOB.TabIndex = 0
+        Me.lblDOB.Text = "Date of Birth:"
+        '
+        'lblToday
+        '
+        Me.lblToday.AutoSize = True
+        Me.lblToday.Location = New System.Drawing.Point(9, 87)
+        Me.lblToday.Name = "lblToday"
+        Me.lblToday.Size = New System.Drawing.Size(40, 13)
+        Me.lblToday.TabIndex = 1
+        Me.lblToday.Text = "Today:"
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(127, 41)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 2
+        '
+        'DateTimePicker2
+        '
+        Me.DateTimePicker2.Location = New System.Drawing.Point(127, 81)
+        Me.DateTimePicker2.Name = "DateTimePicker2"
+        Me.DateTimePicker2.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker2.TabIndex = 3
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(79, 153)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(248, 36)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 124)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(519, 301)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents DateTimePicker2 As DateTimePicker
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents lblToday As Label
+    Friend WithEvents lblDOB As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Age_DOB_Calculator.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Dim datevalue1 As Date
+    Dim datevalue2 As Date
+    Dim datet As TimeSpan
+    Dim diff As Double
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        datevalue1 = Me.DateTimePicker1.Value.ToShortDateString
+        datevalue2 = Me.DateTimePicker2.Value.ToShortDateString
+        datet = (datevalue2 - datevalue1)
+        diff = datet.Days
+        Me.Label3.Text = ("Your age is" + Str(Int(diff / 365)) + " Years")
+        diff = diff Mod 365
+        Me.Label3.Text = Me.Label3.Text + (Str(Int(diff / 30)) + " Months")
+        diff = diff Mod 30
+        Me.Label3.Text = Me.Label3.Text + (Str(Int(diff)) + " Days")
+    End Sub
+End Class
